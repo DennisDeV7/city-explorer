e4b15d849dc45ae1f3f3ef0ddc2bb7fbd13bb6da
Dennis DeVries <7dennisdv7@gmail.com>
2022-06-21 13:59:56 -0400

    adding corrections to lab 06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 import './App.css';
 
 class App extends React.Component {
@@ -9,6 +10,8 @@ class App extends React.Component {
     this.state= {
       city: '',
       cityData: {},
+      error: false,
+      errorMessage: ''
     };
   }
 
@@ -20,14 +23,23 @@ class App extends React.Component {
 
   handleCitySubmit = async (e) => {
     e.preventDefault();
-    // make my request to my API
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
-    let cityInfo = await axios.get(url);
-    console.log(cityInfo.data);
-    this.setState({
-      cityData: cityInfo.data[0],
-    })
-    
+    try{    
+      // make my request to my API
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+      let cityInfo = await axios.get(url);
+      let cityMap = await `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=11`;
+      console.log(cityInfo.data);
+      this.setState({
+        cityData: cityInfo.data[0],
+        cityMap: cityMap
+      });
+    }
+    catch(error){
+      this.setState({
+        error: true,
+        errorMessage: `An Error occurred: ${error.response.status}`
+      })
+    }
   }
 
   render() {
@@ -41,11 +53,13 @@ class App extends React.Component {
           </label>
           <button>Explore!</button>
         </form>
+        {this.state.error?<Alert variant="danger">{this.state.errorMessage}</Alert>:
         <Card>
           <Card.Text>City: {this.state.cityData.display_name}</Card.Text>
           <Card.Text>Latitude: {this.state.cityData.lat}</Card.Text>
           <Card.Text>Longitude: {this.state.cityData.lon}</Card.Text>
-        </Card>
+          <Card.Img src={this.state.cityMap}/>
+        </Card>}
       </>
     )  
   };
